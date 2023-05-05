cb6b5a3630f0247f630567f7dbe74b024be7fb9e
J.A.dev <83449460+aysuskala@users.noreply.github.com>
2023-05-05 11:32:05 +0800

    Update [slug].js

--- pages/news/category/[slug].js
+++ pages/news/category/[slug].js
@@ -31,7 +31,7 @@ const CategoryPost = ({ posts }) => {
 export default CategoryPost;
 
 // Fetch data at build time
-export async function getServerSideProps({ params }) {
+export async function getStaticProps({ params }) {
   const posts = await getCategoryPost(params.slug);
 
   return {
